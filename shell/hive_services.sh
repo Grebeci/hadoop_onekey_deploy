5c1b24d884f26d561878c4394abbf646c6553edd
Anna Wilson <31677997+Grebeci@users.noreply.github.com>
2024-05-09 23:24:08 +0800

    Update hive_services.sh
    增加 hive-metastore启动逻辑

--- shell/hive_services.sh
+++ shell/hive_services.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-CLUSTER="hadoop105"
+CLUSTER='hadoop102'
+
+# 如果是 root 用户, 直接退出
+if [ $USER == "root" ]; then
+  echo "当前用户是 root 用户, 请切换普通用户 bigdata 后执行脚本"
+  exit 1
+fi
+
 IFS="," read -r -a Hosts <<<"$CLUSTER"
 
 hive_start() {
   hdfs dfsadmin -safemode wait
   xcall -w "${Hosts[0]}" "nohup hive --service hiveserver2 1>/dev/null 2>&1 &"
+  xcall -w "${Hosts[0]}" "nohup hive --service metastore 1>/dev/null 2>&1 &"
   while ! nc -z ${Hosts[0]} 10000; do sleep 1; done
 }
 
 hive_stop() {
   xcall -w "${Hosts[0]}" "ps -ef | grep -i hiveserver2 | grep -v grep | awk '{print \$2}' | xargs -n1 kill"
+  xcall -w "${Hosts[0]}" "ps -ef | grep -i metastore | grep -v grep | awk '{print \$2}' | xargs -n1 kill"
 }
 
 case $1 in
@@ -26,6 +35,7 @@ restart)
   ;;
 status)
   if nc -z ${Hosts[0]} 10000; then echo "hiveserver2正在运行"; else echo "hiveserver2未启动成功"; fi
+  if nc -z ${Hosts[0]} 9083; then echo "metastore正在运行"; else echo "metastore未启动成功"; fi
   ;;
 *) ;;
 esac
